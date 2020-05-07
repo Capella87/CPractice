800c19183590f516b52112b75d352b3866fe4a29
Capella87 <contact@capelladev.com>
2020-05-07 17:28:37 +0900

    Add codes at Chapter 6 assignment

---
+++ Chapter6_hw/chap6_hw_4-2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int t;
+    int input, cnt1 = 0, cnt2 = 0, cnt3 = 0;
+
+    scanf("%d", &t);
+    scanf("%d", &input);
+
+    while (input != 0)
+    {
+        while (input != 0)
+        {
+            if (input % 10 == t)
+                cnt1++;
+            else if (input % 10 < t)
+                cnt2++;
+            else if (input % 10 > t)
+                cnt3++;
+            input /= 10;
+        }
+        scanf("%d", &input);
+    }
+    printf("%d %d %d", cnt1, cnt2, cnt3);
+    return 0;
+}
