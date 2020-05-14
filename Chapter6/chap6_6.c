cbb024e17baada9d72f03e5c5b8565bd12b571cd
Capella87 <ins9759@gmail.com>
2020-05-14 23:56:59 +0900

    Fix errors

--- Chapter6/chap6_6.c
+++ Chapter6/chap6_6.c
@@ -10,9 +10,7 @@ int main(void)
     while (i < n)
     {
         for (j = 1; j <= n; j++)
-        {
             printf(" %d", (i + j) % 10); // 숫자 출력; 첫번째 부분에서 줄 번호에 맞게 출력, 10이 넘는 경우 0으로 출력
-        }
         printf("\n");
         i++;
     }
